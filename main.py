a494f5ec41b128dec94e4de7df7aff72c3098735
ehsan2793 <ehsan2793@gmail.com>
2022-02-23 21:35:56 -0800

    game complete

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from turtle import Screen
 from paddle import Paddle
 from ball import Ball
+from scoreBoard import ScoreBoard
 import time
 
 screen = Screen()
@@ -12,6 +13,10 @@ screen.tracer(0)
 
 r_paddle = Paddle(365)
 l_paddle = Paddle(-375)
+
+r_score = ScoreBoard(90)
+l_score = ScoreBoard(-90)
+
 ball = Ball()
 
 screen.onkey(r_paddle.go_up, "Up")
@@ -20,25 +25,29 @@ screen.onkey(l_paddle.go_up, "w")
 screen.onkey(l_paddle.go_down, "s")
 
 game_is_running = True
-
+fast = 0.1
 while game_is_running:
     screen.update()
     ball.move()
-    time.sleep(0.1)
+    time.sleep(fast)
 
     if ball.ycor() > 285 or ball.ycor() < -285:
         ball.bounce_from_wall()
     #   Detect collisions with right paddle
     if ball.distance(r_paddle) < 32 and ball.xcor() > 305 or ball.distance(l_paddle) < 32 and ball.xcor() < -305:
         ball.bounce_from_paddle()
-        r_paddle.add_to_speed()
-        l_paddle.add_to_speed()
+        fast /= 1.1
 
     # when R missed  the ball
     if ball.xcor() > 375:
         ball.start_in_center()
+        l_score.add()
+        fast = 0.1
 
     # L paddle missed the ball
     if ball.xcor() < -385:
         ball.start_in_center()
+        r_score.add()
+        fast = 0.1
+
 screen.exitonclick()
---
+++ scoreBoard.py
@@ -0,0 +1,23 @@
+from turtle import Turtle
+
+
+class ScoreBoard(Turtle):
+    def __init__(self, x, y=250):
+        super().__init__()
+
+        self.color('white')
+        self.penup()
+        self.hideturtle()
+        self.score = 0
+        self.position = (x, y)
+        self.update_score()
+
+
+    def update_score(self):
+        self.setposition(self.position)
+        self.clear()
+        self.write(arg=f' {self.score}', move=True, font=('times', 40, "bold"))
+
+    def add(self):
+        self.score += 1
+        self.update_score()
